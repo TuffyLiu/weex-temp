b28137d4fa78cb837b5d8df64352fc69e7cb90ad
tuffy <tuffy@hellotalk.com>
2018-07-10 15:38:40 +0800

    init weex-temp

---
+++ configs/webpack.weex.conf.js
@@ -0,0 +1,335 @@
+const path = require('path');
+const fs = require('fs-extra');
+const webpack = require('webpack');
+const config = require('./config');
+const helper = require('./helper');
+const glob = require('glob');
+const vueLoaderConfig = require('./vue-loader.conf');
+const vueWebTemp = helper.rootNode(config.templateDir);
+const isWin = /^win/.test(process.platform);
+const weexEntry = {};
+
+
+const os = require('os');
+
+
+/**
+ * Webpack Plugins
+ */
+const webpackMerge = require('webpack-merge'); // used to merge webpack configs
+const UglifyJsparallelPlugin = require('webpack-uglify-parallel');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+
+/**
+ * Generate multiple entrys
+ * @param {Array} entry
+ */
+const generateMultipleEntrys = (entry) => {
+  let entrys = Object.keys(entry);
+  console.log("entrys", entrys);
+  // exclude vendor entry.
+  entrys = entrys.filter(entry => entry !== 'vendor' );
+  const htmlPlugin = entrys.map(name => {
+    return new HtmlWebpackPlugin({
+      filename: name + '.html',
+      template: helper.rootNode(`web/index.html`),
+      isDevServer: true,
+      chunksSortMode: 'dependency',
+      inject: true,
+      chunks: [name],
+      // production
+      minimize: true
+    })
+  })
+  return htmlPlugin;
+}
+
+
+// Wraping the entry file for native.
+const getNativeEntryFileContent = (entryPath, vueFilePath) => {
+  let relativeVuePath = path.relative(path.join(entryPath, '../'), vueFilePath);
+  let contents = '';
+  if (isWin) {
+    relativeVuePath = relativeVuePath.replace(/\\/g, '\\\\');
+  }
+  contents += `import App from '${relativeVuePath}'
+App.el = '#root'
+new Vue(App)
+`;
+
+  return contents;
+}
+
+
+
+const getBaseFile = ( ) => {
+    const entryPath =  path.join(vueWebTemp, 'entry' + '.js');
+    let relativeEntryPath = helper.root(config.entryFilePath);
+    let entryContents = fs.readFileSync(relativeEntryPath).toString();
+    fs.outputFileSync(entryPath, entryContents);
+    weexEntry['entry'] = entryPath;
+}
+// Retrieve entry file mappings by function recursion
+const getEntryFile = (dir) => {
+  dir = dir || config.sourceDir;
+  const enrtys = glob.sync(`${dir}/${config.entryFilter}`, config.entryFilterOptions);
+  enrtys.forEach(entry => {
+    const extname = path.extname(entry);
+    const basename = entry.replace(`${dir}/`, '').replace(extname, '');
+    const templatePathForNative = path.join(vueWebTemp, basename + '.js');
+    fs.outputFileSync(templatePathForNative, getNativeEntryFileContent(templatePathForNative, entry));
+    weexEntry[basename] = templatePathForNative;
+  })
+}
+
+// Generate an entry file array before writing a webpack configuration
+getEntryFile();
+getBaseFile();
+
+const createLintingRule = () => ({
+  test: /\.(js|vue)$/,
+  loader: 'eslint-loader',
+  enforce: 'pre',
+  include: [helper.rootNode('src'), helper.rootNode('test')],
+  options: {
+    formatter: require('eslint-friendly-formatter'),
+    emitWarning: !config.dev.showEslintErrorsInOverlay
+  }
+})
+const useEslint = config.dev.useEslint ? [createLintingRule()] : []
+
+/**
+ * Plugins for webpack configuration.
+ */
+const plugins = [
+  /*
+   * Plugin: BannerPlugin
+   * Description: Adds a banner to the top of each generated chunk.
+   * See: https://webpack.js.org/plugins/banner-plugin/
+   */
+  new webpack.BannerPlugin({
+    banner: '// { "framework": "Vue"} \n',
+    raw: true,
+    exclude: 'Vue'
+  })
+];
+
+
+// Config for compile jsbundle for native.
+const weexConfig = {
+  entry: weexEntry,
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].js'
+  },
+  /**
+   * Options affecting the resolving of modules.
+   * See http://webpack.github.io/docs/configuration.html#resolve
+   */
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': helper.resolve('src')
+    }
+  },
+  /*
+   * Options affecting the resolving of modules.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#module
+   */
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader'
+        }],
+        exclude: config.excludeModuleReg
+      },
+      {
+        test: /\.vue(\?[^?]+)?$/,
+        use: [{
+          loader: 'weex-loader',
+          options: vueLoaderConfig({useVue: false})
+        }],
+        exclude: config.excludeModuleReg
+      }
+    ]
+  },
+  /*
+   * Add additional plugins to the compiler.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#plugins
+   */
+  plugins: plugins,
+  /*
+  * Include polyfills or mocks for various node stuff
+  * Description: Node configuration
+  *
+  * See: https://webpack.github.io/docs/configuration.html#node
+  */
+  node: config.nodeConfiguration
+};
+
+
+/**
+ * Webpack configuration for weex.
+ */
+const weexConfig2 = webpackMerge(weexConfig[1], {
+  /**
+   * Options affecting the output of the compilation.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#output
+   */
+  output: {
+    /**
+     * The output directory as absolute path (required).
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-path
+     */
+    path: helper.rootNode('dist'),
+    /**
+     * Specifies the name of each output file on disk.
+     * IMPORTANT: You must not specify an absolute path here!
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-filename
+     */
+    filename: '[name].js'
+  },
+  /*
+   * Add additional plugins to the compiler.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#plugins
+   */
+  plugins: [
+    /*
+     * Plugin: UglifyJsparallelPlugin
+     * Description: Identical to standard uglify webpack plugin
+     * with an option to build multiple files in parallel
+     *
+     * See: https://www.npmjs.com/package/webpack-uglify-parallel
+     */
+    new UglifyJsparallelPlugin({
+      workers: os.cpus().length,
+      mangle: true,
+      compressor: {
+        warnings: false,
+        drop_console: true,
+        drop_debugger: true
+      }
+    })
+  ]
+})
+
+const productionConfig = webpackMerge(weexConfig, {
+  /**
+   * Developer tool to enhance debugging
+   *
+   * See: http://webpack.github.io/docs/configuration.html#devtool
+   * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
+   */
+  devtool: config.prod.devtool,
+  /**
+   * Options affecting the output of the compilation.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#output
+   */
+  output: {
+    /**
+     * The output directory as absolute path (required).
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-path
+     */
+    path: helper.rootNode('release/web'),
+    /**
+     * Specifies the name of each output file on disk.
+     * IMPORTANT: You must not specify an absolute path here!
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-filename
+     */
+    filename: '[name].[chunkhash].bundle.js',
+    /**
+     * The filename of the SourceMaps for the JavaScript files.
+     * They are inside the output.path directory.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-sourcemapfilename
+     */
+    sourceMapFilename: '[name].[chunkhash].bundle.map',
+    /**
+     * The filename of non-entry chunks as relative path
+     * inside the output.path directory.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-chunkfilename
+     */
+    chunkFilename: '[id].[chunkhash].chunk.js'
+  },
+  /*
+   * Add additional plugins to the compiler.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#plugins
+   */
+  plugins: [
+    /**
+     * Plugin: webpack.DefinePlugin
+     * Description: The DefinePlugin allows you to create global constants which can be configured at compile time.
+     *
+     * See: https://webpack.js.org/plugins/define-plugin/
+     */
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': config.prod.env
+      }
+    }),
+    /*
+     * Plugin: UglifyJsPlugin
+     * Description: Minimize all JavaScript output of chunks.
+     * Loaders are switched into minimizing mode.
+     *
+     * See: https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+     */
+    ...generateMultipleEntrys(weexConfig.entry),
+    /*
+     * Plugin: HtmlWebpackPlugin
+     * Description: Simplifies creation of HTML files to serve your webpack bundles.
+     * This is especially useful for webpack bundles that include a hash in the filename
+     * which changes every compilation.
+     *
+     * See: https://github.com/ampedandwired/html-webpack-plugin
+     */
+    new HtmlWebpackPlugin({
+      template: 'web/index.html',
+      chunksSortMode: 'dependency',
+      inject: 'head'
+    }),
+    /*
+     * Plugin: ScriptExtHtmlWebpackPlugin
+     * Description: Enhances html-webpack-plugin functionality
+     * with different deployment options for your scripts including:
+     *
+     * See: https://github.com/numical/script-ext-html-webpack-plugin
+     */
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer'
+    }),
+    /*
+     * Plugin: UglifyJsparallelPlugin
+     * Description: Identical to standard uglify webpack plugin
+     * with an option to build multiple files in parallel
+     *
+     * See: https://www.npmjs.com/package/webpack-uglify-parallel
+     */
+    new UglifyJsparallelPlugin({
+      workers: os.cpus().length,
+      mangle: true,
+      compressor: {
+        warnings: false,
+        drop_console: true,
+        drop_debugger: true
+      }
+    })
+  ]
+});
+
+module.exports = [weexConfig, productionConfig];
+
